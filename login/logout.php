1c193916b543f18e991f3024e1d6da5b89eadfbb
wavewalker5357 <p.vlnas@email.cz>
2014-04-27 20:50:50 +0300

    5 committ

--- login/logout.php
+++ login/logout.php
@@ -4,9 +4,8 @@
 
  //this makes the time in the past to destroy the cookie
 
- setcookie('ID_Eventime', 'gone', $past);
-
- setcookie('Key_Eventime', 'gone', $past);
+ setcookie('ID_Eventime', gone, $past);
+ setcookie('Key_Eventime', gone, $past);
 
  header("Location: login.php");
 
