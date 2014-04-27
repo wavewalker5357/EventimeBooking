30e2d84e0ce951367150a91aedfbaa1719d2b691
wavewalker5357 <p.vlnas@email.cz>
2014-04-27 21:40:51 +0300

    gone committ

--- login/logout.php
+++ login/logout.php
@@ -2,8 +2,8 @@
 var_dump('', $_COOKIE, '');
  $past = time() - 100;
 
- setcookie('ID_Eventime', gone, $past, '/');
- setcookie('Key_Eventime', gone, $past, '/');
+ setcookie('ID_Eventime', 'gone', $past, '/');
+ setcookie('Key_Eventime', 'gone', $past, '/');
 
 
  header("Location: ../index.php");
